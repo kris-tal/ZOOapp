4299431128b05feeabaec1a056fbb49e21862e21
annaszpa <Annaa.szpak@student.uj.edu.pl>
2024-06-03 19:56:13 +0200

    f

--- app/src/main/java/app/zoo/database/PsqlManager.java
+++ app/src/main/java/app/zoo/database/PsqlManager.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 public class PsqlManager {
     private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/mydatabase";
     private static final String DATABASE_USER = "postgres";
-    private static final String DATABASE_PASSWORD = "12345a";
+    private static final String DATABASE_PASSWORD = ".";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
