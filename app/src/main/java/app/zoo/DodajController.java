b553548979a778fddcc186368213e89b026cea84
annaszpa <Annaa.szpak@student.uj.edu.pl>
2024-06-08 13:32:59 +0200

    dodaj

--- app/src/main/java/app/zoo/DodajController.java
+++ app/src/main/java/app/zoo/DodajController.java
@@ -48,57 +48,88 @@ public class DodajController extends ToolBarController {
     private ComboBox tabelaComboBox;
 
     @Override
-    public void initialize() {
-        super.initialize();
-        Connection connection = null;
-        Statement statement = null;
-        ResultSet resultSet = null;
+public void initialize() {
+    super.initialize();
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet resultSet = null;
+    try {
+        connection = PsqlManager.getConnection();
+        statement = connection.createStatement();
+
+        // Zapytanie SQL do pobrania listy wszystkich tabel w bieżącej bazie danych
+        String queryTables = "SELECT table_name FROM information_schema.tables WHERE table_schema='public'";
+        resultSet = statement.executeQuery(queryTables);
+        
+        // Wczytanie nazw tabel do ComboBoxa
+        while (resultSet.next()) {
+            tabelaComboBox.getItems().add(resultSet.getString("table_name"));
+        }
+
+        // Listener dla ComboBoxa, aby załadować metadane wybranej tabeli
+        tabelaComboBox.getSelectionModel().selectedItemProperty().addListener((options, oldValue, newValue) -> {
+            loadTableMetadata(newValue.toString());
+        });
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    } finally {
         try {
-            // Ustaw połączenie z bazą danych korzystając z klasy PsqlManager
-            connection = PsqlManager.getConnection();
-            statement = connection.createStatement();
-            
-            // Zapytanie SQL do pobrania metadanych (nazw kolumn)
-            String query = "SELECT * FROM nazwa_tabeli WHERE 1=0"; // Dostosuj nazwę tabeli
-            resultSet = statement.executeQuery(query);
-            
-            // Uzyskaj metadane
-            ResultSetMetaData metaData = resultSet.getMetaData();
-            int columnNumber = metaData.getColumnCount();
-            
-            // Zapisz nazwy kolumn
-            ArrayList<String> columnNames = new ArrayList<>();
-            for (int i = 1; i <= columnNumber; i++) {
-                columnNames.add(metaData.getColumnName(i));
-            }
-            
-            iloscCheckBox.setDisable(true);
-            iloscTextField.setDisable(true);
-            TextField[] pola = {pole1, pole2, pole3, pole4, pole5, pole6};
-            Label[] etykiety = {label1, label2, label3, label4, label5, label6};
-            for (TextField pole : pola) {
-                pole.setDisable(true);
-            }
-            
-            // Ustawienie dostępnych pól i etykiet na podstawie metadanych
-            for (int i = 0; i < columnNumber; i++) {
-                if (i < pola.length) { // Zabezpieczenie przed wyjściem poza zakres
-                    pola[i].setDisable(false);
-                    etykiety[i].setText(columnNames.get(i));
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (resultSet != null) resultSet.close();
-                if (statement != null) statement.close();
-                if (connection != null) connection.close();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+            if (resultSet != null) resultSet.close();
+            if (statement != null) statement.close();
+            if (connection != null) connection.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
+}
+
+private void loadTableMetadata(String tableName) {
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet resultSet = null;
+    try {
+        connection = PsqlManager.getConnection();
+        statement = connection.createStatement();
+
+        // Zapytanie SQL do pobrania metadanych (nazw kolumn) dla wybranej tabeli
+        String query = "SELECT * FROM " + tableName + " WHERE 1=0";
+        resultSet = statement.executeQuery(query);
+
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        int columnNumber = metaData.getColumnCount();
+
+        ArrayList<String> columnNames = new ArrayList<>();
+        for (int i = 1; i <= columnNumber; i++) {
+            columnNames.add(metaData.getColumnName(i));
+        }
+
+        iloscCheckBox.setDisable(true);
+        iloscTextField.setDisable(true);
+        TextField[] pola = {pole1, pole2, pole3, pole4, pole5, pole6};
+        Label[] etykiety = {label1, label2, label3, label4, label5, label6};
+        for (TextField pole : pola) {
+            pole.setDisable(true);
+        }
+
+        for (int i = 0; i < columnNumber; i++) {
+            if (i < pola.length) {
+                pola[i].setDisable(false);
+                etykiety[i].setText(columnNames.get(i));
+            }
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    } finally {
+        try {
+            if (resultSet != null) resultSet.close();
+            if (statement != null) statement.close();
+            if (connection != null) connection.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
 
     static public void openDodaj(Stage stage) {
         SceneLoader.loadScene("dodaj.fxml", stage);
